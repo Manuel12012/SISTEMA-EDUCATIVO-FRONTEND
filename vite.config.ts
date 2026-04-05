32fe5e770ad978d72ab3836539bd8304add752d5
Manuel Anthony Meza Rivas <manuelmezarivas120@gmail.com>
2026-04-04 20:47:41 -0500

    keys front

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),       // raíz
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')) // raíz
+    },
+    port: 3000
+  }
 })
